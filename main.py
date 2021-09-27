1d520f6f970e7cf6ced3b8f01451e8806cfa6f11
danny <dannypolyakov95@gmail.com>
2021-09-27 20:51:45 +0300

    dev_br

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from docx import Document
 import docx2txt
 import os
+import re
 
 # Passing docx file to process function
 path_to_file = '/home/danny/Downloads/Документ.docx'
@@ -10,29 +11,43 @@ text = docx2txt.process(path_to_file)
 with open("output.txt", "w") as text_file:
     print(text, file=text_file)
 
-# Math logic and operations
+# Preparing our values from strings for future calculations
 with open('output.txt') as f:
     lines = []
+    values = []
+    final_list = []
     for line in f:
         pure_line = line.strip('\n')
         lines.append(pure_line)
         while "" in lines:
             lines.remove("")
-
+    lines.pop(0)
+    for element in lines:  # getting only numbers from string
+        values += re.findall(r"[-+]?\d*\.\d+|\d+", element)
+    for element in values:
+        if "." not in element:  # getting integers and floats
+            final_list.append(int(element))
+        else:
+            final_list.append(float(element))
     """
-    Math operations here
+    Setting variables to our values from .docx
     """
-    sum0_1 = int(lines[0]) + int(lines[1])
-    dev2_01 = int(lines[2]) - sum0_1
+    a = final_list[0]  # value a
+    b = final_list[1]  # value b
+    c = final_list[2]  # value c
+    # etc
+    """
+        Math operations here
+    """
+    d = (a + b) / c
+    d_val = " N, J, kgf"  # specify dimension
     f.close()
-
-print(lines)
 
 
 # Creating result.txt file to save calculations
 file = open("result.txt", "w")
-file.write(str(sum0_1) + '\n')
-file.write(str(dev2_01) + '\n')
+file.write("Results:\n" + '\n')
+file.write("d = " + str(d) + d_val + '\n')
 file.close()
 
 
@@ -42,7 +57,7 @@ doc = Document()
 with open("result.txt", 'r', encoding='utf-8') as openfile:
     line = openfile.read()
     doc.add_paragraph(line)
-    doc.save("result.docx")
+    doc.save("Results of calculations.docx")
     # doc.save("Your wanted location" + "result.docx") - save directly to preferable directory
 
 
